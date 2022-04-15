fdb6272e027b891041ac7da8eccc4f9b928eaf53
dongfang <1790956510@qq.com>
2022-04-15 14:52:03 +0800

    feat(中间件): 添加中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,21 @@
  * @Author: zhaodongfeng
  * @Date: 2022-04-14 10:02:26
  * @LastEditors: zhaodongfeng
- * @LastEditTime: 2022-04-15 10:44:48
+ * @LastEditTime: 2022-04-15 14:16:01
  */
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { LoggerMiddleWare } from './modules/common/middleware/logger.middleware';
 import { HelloModule } from './modules/hello/hello.module';
 
 @Module({
   imports: [HelloModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    // 为hello路由添加中间间
+    consumer
+      .apply(LoggerMiddleWare)
+      .exclude({ path: 'hello', method: RequestMethod.POST })
+      .forRoutes('');
+  }
+}
---
+++ src/modules/common/middleware/logger.middleware.ts
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion:
+ * @Author: zhaodongfeng
+ * @Date: 2022-04-15 13:49:44
+ * @LastEditors: zhaodongfeng
+ * @LastEditTime: 2022-04-15 14:05:46
+ */
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleWare implements NestMiddleware {
+  use(req: Request, res: Response, next: (error?: any) => void) {
+    const { method, path } = req;
+    console.log(`${method} ${path}`);
+    next();
+  }
+}
